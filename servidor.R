d6bed1ddc7caaaad2a345ae3128689815887427e
Sal <salvocalfonseca@hotmail.com>
2020-10-19 15:56:29 -0300

    Incluindo o projeto R

---
+++ servidor.R
@@ -0,0 +1,106 @@
+#load all packages
+library(httr)
+library(future)
+library(promises)
+library(jsonlite)
+library(ipc)
+library(stringr)
+library(sf)
+library(snowfall)
+library(raster)
+library(geosphere)
+library(rgeos)
+
+#R working with multithreads
+plan(multisession)
+
+#var global
+interruptor <- NULL
+syscronize  <- NULL
+
+#Create function output
+printf <- function(...){ print(paste0(...))}
+
+# Create function service
+openService <- function(callback){
+  
+  if(!is.null(interruptor)) return(FALSE)
+  
+  interruptor <<- ipc::AsyncInterruptor$new()
+  syscronize  <<- ipc::queue()
+  syscronize$consumer$start(100)
+  
+  #neutro function
+  futuroFunction <- function(x){callback(x)}
+  
+  config  <- fromJSON('config.json')
+  
+  printf("Http url: ",config$url)
+
+  future({
+    
+    message   <- ''
+    running   <- TRUE
+    
+    repeat({
+      
+      if(!running)
+          break
+      
+       tryCatch({
+       
+        request <- httr::GET(str_trim(config$url),accept('text/plain'))
+        wkb     <- content(request, "text")
+        
+        #send request to callback
+        syscronize$producer$fireCall(name = 'futuroFunction',list(url = config$url,wkb = data,time = Sys.time()))
+        
+        #check if service was stoped
+        interruptor$execInterrupts()
+        
+      },error = function(e){
+        running <<- FALSE
+        message   <<- e
+      })
+ 
+     
+    })
+    #print message
+    message
+    
+  })%...>%(function(x) printf(x))
+  
+  return(TRUE)
+}
+
+# Create function stop service
+stopService <- function(){
+  
+  if(is.null(interruptor)) return(FALSE)
+  
+  #stop
+  interruptor$interrupt('Stop')
+  syscronize$consumer$stop()
+  syscronize$destroy()
+  
+  interruptor <<- NULL
+  syscronize  <<- NULL
+  
+  return(TRUE)
+}
+
+# Run service
+openService(function(obj){ # callback request
+  
+  #make new future
+  future({
+    
+    geometry.union <- st_union(st_cast(st_as_sf(readWKT(wkb_wkt(obj$wkb))),to = 'POLYGON'))
+    new.wkb        <- toupper(paste0(st_as_binary(geometry.union),collapse = ''))
+    #Send new object WKB UNION
+    httr::POST(obj$url,accept('text/plain'),body = new.wkb)
+    
+  })
+  
+})
+
